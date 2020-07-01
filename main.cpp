88bb8eb168fc970a1779a8b03ecc978d569f4e5c
BernieTsou <tsoubernie@gmail.com>
2020-07-01 18:23:22 +0800

    因pcd檔案中的點雲z值可能為負值，如用原本的方法可能會導致負的點雲z值的深度值永遠都是零。 所以這邊將方法更改，加入shift的概念，解決上述問題。

--- main.cpp
+++ main.cpp
@@ -32,11 +32,29 @@ void saveDepthToPNG (const std::string &file_name, const pcl::PointCloud<PointT>
     depth.is_dense = cloud.is_dense;
     depth.points.resize (depth.width * depth.height);
 
-    double max = 0, min = 0;
+    //bad point
+    const float bad_point = std::numeric_limits<float>::quiet_NaN();
+    float max = bad_point, min = bad_point;
 
-    for (int i = 0; i < (int) cloud.points.size(); i++) {
-        if(cloud.points[i].z > max) max = cloud.points[i].z;
-        if(cloud.points[i].z < min) min = cloud.points[i].z;
+    //先找出上下限值
+    for (int i = 0; i < (int) cloud.points.size(); i++)
+    {
+        if(std::isfinite(cloud.points[i].z))
+        {
+            //qDebug()<<"cloud.points[i].z != bad_point";
+            //qDebug()<<"cloud.points[i].z:"<<cloud.points[i].z;
+            if(!std::isfinite(max) && !std::isfinite(min))  //第一次找到finite的z值，就將值初始化給max和min
+            {
+                max = cloud.points[i].z;
+                min = cloud.points[i].z;
+                //qDebug()<<"max == bad_point && min == bad_point";
+            }
+            else
+            {
+                if(cloud.points[i].z > max) max = cloud.points[i].z;
+                if(cloud.points[i].z < min) min = cloud.points[i].z;
+            }
+        }
     }
 
     qDebug()<<"max:"<<max;
@@ -45,40 +63,23 @@ void saveDepthToPNG (const std::string &file_name, const pcl::PointCloud<PointT>
     float scale = (max - min) / 256;
     qDebug()<<"scale:"<<scale;
 
-    for (int i = 0; i < (int) cloud.points.size(); i++) {
-        float t = cloud.points[i].z / scale;
-        if(i == 6881)
-        {
-            qDebug()<<"t at 6881(1):"<<t;
-            qDebug()<<"t:"<<cloud.points[i].z / scale;
-        }
+    for (int i = 0; i < (int) cloud.points.size(); i++)
+    {
+        float t = (cloud.points[i].z - min) / scale;  //將z值做shift
 
         // make sure pixel value between 0 ~ 255
-        t = (t > 255) ? 255 : (t < 0) ? 0 : t;
-        if(i == 6881)
-        {
-            qDebug()<<"t at 6881(2):"<<t;
-        }
-
+        t = (t > 255) ? 255 : (t < 0 || !std::isfinite(t)) ? 0 : t;
 
         //depth.points[i].z = t;
         depth.points[i].x = cloud.points[i].x;
         depth.points[i].y = cloud.points[i].y;
         depth.points[i].z = cloud.points[i].z;
         depth.points[i].rgba = (((int)t << 16) | ((int)t << 8) | (int)t);
-
-
-        if(i == 6881)
-        {
-            qDebug()<<"depth.points[6881].z:"<<depth.points[i].z;
-            qDebug()<<"t at 6881(3):"<<t;
-            qDebug()<<"depth.points[i].rgba:"<<depth.points[i].rgba;
-        }
     }
 
 
     pcl::io::savePNGFile(file_name, depth);
 
     // Save cloud data
-    pcl::io::savePCDFileASCII ("depth.pcd", depth);
+    //pcl::io::savePCDFileASCII ("depth.pcd", depth);
 }
